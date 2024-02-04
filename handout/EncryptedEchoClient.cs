5d1af3ce484f43a501b9cbb5754f9c442716e2e1
NotStanley4330 <you@example.com>
2024-02-04 00:50:54 +0000

    Finished lab

--- handout/EncryptedEchoClient.cs
+++ handout/EncryptedEchoClient.cs
@@ -1,5 +1,7 @@
 ﻿using System.Security.Cryptography;
 using System.Text.Json;
+using System.Text;
+using System.IO;
 using Microsoft.Extensions.Logging;
 
 /// <summary>
@@ -7,6 +9,7 @@ using Microsoft.Extensions.Logging;
 /// </summary>
 internal sealed class EncryptedEchoClient : EchoClientBase {
 
+    RSA rsa;
     /// <summary>
     /// Logger to use in this class.
     /// </summary>
@@ -14,48 +17,67 @@ internal sealed class EncryptedEchoClient : EchoClientBase {
         Settings.LoggerFactory.CreateLogger<EncryptedEchoClient>()!;
 
     /// <inheritdoc />
-    public EncryptedEchoClient(ushort port, string address) : base(port, address) { }
-
-    /// <inheritdoc />
-    public override void ProcessServerHello(string message) {
-        // todo: Step 1: Get the server's public key. Decode using Base64.
-        // Throw a CryptographicException if the received key is invalid.
+    public EncryptedEchoClient(ushort port, string address) : base(port, address) 
+    { 
+        rsa = RSA.Create();
     }
 
     /// <inheritdoc />
-    public override string TransformOutgoingMessage(string input) {
-        byte[] data = Settings.Encoding.GetBytes(input);
+    public override void ProcessServerHello(string message) 
+    {
+        //Step 1: Get the server's public key. Decode using Base64.
+        // Throw a CryptographicException if the received key is invalid.
+        byte[] publicKeyBytes = Convert.FromBase64String(message);
+        rsa.ImportRSAPublicKey(publicKeyBytes, out _);
+        Logger.LogInformation("Public key loaded from server hello");
+    }
 
-        // todo: Step 1: Encrypt the input using hybrid encryption.
+    /// <inheritdoc />
+    public override string TransformOutgoingMessage(string input) 
+    {
+
+        //Step 1: Encrypt the input using hybrid encryption.
         // Encrypt using AES with CBC mode and PKCS7 padding.
         // Use a different key each time.
+        Aes aes = Aes.Create();
+        aes.GenerateIV();
+        aes.GenerateKey();
+        byte[] plainBytes = Encoding.UTF8.GetBytes(input);
+        byte[] encryptedInput = aes.EncryptCbc(plainBytes, aes.IV, PaddingMode.PKCS7);
 
-        // todo: Step 2: Generate an HMAC of the message.
+        //Step 2: Generate an HMAC of the message.
         // Use the SHA256 variant of HMAC.
         // Use a different key each time.
+        HMACSHA256 hmac = new HMACSHA256();
+        byte[] hash = hmac.ComputeHash(plainBytes);
 
-        // todo: Step 3: Encrypt the message encryption and HMAC keys using RSA.
+        //Step 3: Encrypt the message encryption and HMAC keys using RSA.
         // Encrypt using the OAEP padding scheme with SHA256.
+        byte[] aesKeyEncrypted = rsa.Encrypt(aes.Key, RSAEncryptionPadding.OaepSHA256);
+        byte[] hmacKeyEncrypted = rsa.Encrypt(hmac.Key, RSAEncryptionPadding.OaepSHA256);
+
 
         // todo: Step 4: Put the data in an EncryptedMessage object and serialize to JSON.
         // Return that JSON.
-        // var message = new EncryptedMessage(...);
-        // return JsonSerializer.Serialize(message);
-
-        return input;
+        var message = new EncryptedMessage(aesKeyEncrypted, aes.IV, encryptedInput, hmacKeyEncrypted, hash);
+        return JsonSerializer.Serialize(message);
     }
 
     /// <inheritdoc />
     public override string TransformIncomingMessage(string input) {
-        // todo: Step 1: Deserialize the message.
-        // var signedMessage = JsonSerializer.Deserialize<SignedMessage>(input);
+        //Step 1: Deserialize the message.
+        var signedMessage = JsonSerializer.Deserialize<SignedMessage>(input);
 
-        // todo: Step 2: Check the messages signature.
+        //Step 2: Check the messages signature.
         // Use PSS padding with SHA256.
         // Throw an InvalidSignatureException if the signature is bad.
+        bool isValid = rsa.VerifyData(signedMessage.Message, signedMessage.Signature, HashAlgorithmName.SHA256, RSASignaturePadding.Pss);
+        if (!isValid)
+        {
+            throw new InvalidSignatureException("Signature was invalid!");
+        }
 
-        // todo: Step 3: Return the message from the server.
-        // return Settings.Encoding.GetString(signedMessage.Message);
-        return input;
+        //Step 3: Return the message from the server.
+        return Settings.Encoding.GetString(signedMessage.Message);
     }
 }
--- handout/EncryptedEchoServer.cs
+++ handout/EncryptedEchoServer.cs
@@ -1,9 +1,12 @@
 ﻿using System.Security.Cryptography;
+using System.Text;
+using System.Collections;
 using System.Text.Json;
 using Microsoft.Extensions.Logging;
 
 internal sealed class EncryptedEchoServer : EchoServerBase {
 
+    RSA rsa;
     /// <summary>
     /// Logger to use in this class.
     /// </summary>
@@ -11,45 +14,63 @@ internal sealed class EncryptedEchoServer : EchoServerBase {
         Settings.LoggerFactory.CreateLogger<EncryptedEchoServer>()!;
 
     /// <inheritdoc />
-    internal EncryptedEchoServer(ushort port) : base(port) { }
+    internal EncryptedEchoServer(ushort port) : base(port) 
+    { 
+        rsa = RSA.Create(2048); //creates an RSA public key of 2048 bits
+    }
 
     // todo: Step 1: Generate a RSA key (2048 bits) for the server.
            
     /// <inheritdoc />
     public override string GetServerHello() {
-        // todo: Step 1: Send the public key to the client in PKCS#1 format.
+        //Step 1: Send the public key to the client in PKCS#1 format.
+        byte[] publicKey = rsa.ExportRSAPublicKey();
         // Encode using Base64: Convert.ToBase64String
-        return string.Empty;
+        return Convert.ToBase64String(publicKey);
     }
 
     /// <inheritdoc />
     public override string TransformIncomingMessage(string input) {
-        // todo: Step 1: Deserialize the message.
-        // var message = JsonSerializer.Deserialize<EncryptedMessage>(input);
+        //Step 1: Deserialize the message.
+        var message = JsonSerializer.Deserialize<EncryptedMessage>(input);
         
-        // todo: Step 2: Decrypt the message using hybrid encryption.
+        //Step 2: Decrypt the message using hybrid encryption.
+        //first we will decrypt the keys
+        byte[] aesKey = rsa.Decrypt(message.AesKeyWrap, RSAEncryptionPadding.OaepSHA256);
+        byte[] hmacKey = rsa.Decrypt(message.HMACKeyWrap, RSAEncryptionPadding.OaepSHA256);
+        
+        //Do the aes decryption next
+        Aes aes = Aes.Create();
+        aes.Key = aesKey;
+        aes.IV = message.AESIV;
+        byte[] decryptedMessage = aes.DecryptCbc(message.Message, message.AESIV, PaddingMode.PKCS7);
+        string messageText = Encoding.UTF8.GetString(decryptedMessage);
+        
 
-        // todo: Step 3: Verify the HMAC.
-        // Throw an InvalidSignatureException if the received hmac is bad.
+        // Step 3: Verify the HMAC.
+        // Throw an InvalidSignatureException if the received hmac is bad
+        HMACSHA256 hmac = new HMACSHA256(hmacKey);
+        byte[] computedHash = hmac.ComputeHash(decryptedMessage);
+        if (!computedHash.SequenceEqual(message.HMAC))
+        {
+            throw new InvalidSignatureException("Recieved HMAC is bad!");
+        }
 
-        // todo: Step 3: Return the decrypted and verified message from the server.
-        // return Settings.Encoding.GetString(decryptedMessage);
-
-        return input;
+        //Step 3: Return the decrypted and verified message from the server.
+        return Settings.Encoding.GetString(decryptedMessage);
     }
 
     /// <inheritdoc />
     public override string TransformOutgoingMessage(string input) {
         byte[] data = Settings.Encoding.GetBytes(input);
 
-        // todo: Step 1: Sign the message.
+        //Step 1: Sign the message.
         // Use PSS padding with SHA256.
+        byte[] messageSignature = rsa.SignData(data, HashAlgorithmName.SHA256, RSASignaturePadding.Pss);
 
-        // todo: Step 2: Put the data in an SignedMessage object and serialize to JSON.
+        //Step 2: Put the data in an SignedMessage object and serialize to JSON.
         // Return that JSON.
-        // var message = new SignedMessage(...);
-        // return JsonSerializer.Serialize(message);
-
-        return input;
+        var message = new SignedMessage(data, messageSignature);
+        return JsonSerializer.Serialize(message);
     }
 }
